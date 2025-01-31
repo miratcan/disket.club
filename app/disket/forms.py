f994a71730ff43add48412ddc7df13de364e2429
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2025-01-31 16:42:25 +0300

    Do not go gentle into that good night.

--- app/disket/forms.py
+++ app/disket/forms.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
 import zipfile
-import uuid
 from pathlib import Path
 from os.path import join, basename
 from django import forms
@@ -19,16 +18,14 @@ class DisketUploadForm(forms.ModelForm):
 
     def clean_zip_file(self):
         zip_file = self.cleaned_data.get('zip_file')
-        if zip_file.size > 2 * 1024 * 1024:
-            raise forms.ValidationError("The upload size must not exceed 2MB.")
+        if zip_file.size > 1.44 * 1024 * 1024:
+            raise forms.ValidationError("The upload size must not exceed 1.44MB.")
         try:
             with zipfile.ZipFile(zip_file, 'r') as zip_ref:
                 if 'index.html' not in zip_ref.namelist():
                     raise forms.ValidationError("index.html is missing in the uploaded zip file.")
-                
                 zip_folder_name = basename(zip_file.name)
                 base_dir = os.path.join('page', zip_folder_name)
-
                 for file_name in zip_ref.namelist():
                     if file_name.endswith('/'):
                         continue  # Skip directories
